5f36351c46cec1dff02e8e08ef68a049227b25bf
sanjana-poptani54 <sanjanapoptani54@gmail.com>
2022-07-12 13:07:46 +0530

    2 screens added

---
+++ .history/myfirsttry/views_20220709140951.py
@@ -0,0 +1,19 @@
+from http.client import HTTPResponse
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+
+
+def login(request):
+    return render(request,"login.html")
+
+
+def signup(request):
+    return render(request,"signup.html")
+
+
+def cards(request):
+    return render(request,"car")
